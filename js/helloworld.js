d740539b2ad6c374f0ece5a1a29c00e441da9b19
Hunter Schuler <schuler.hunter.l@gmail.com>
2019-06-21 20:55:22 -0400

    Continue building hello world

--- js/helloworld.js
+++ js/helloworld.js
@@ -1,34 +1,34 @@
 helloWorld = {
     python: `print("Hello World")`,
     ruby: `puts "Hello World"`,
-    java: `
-    class HelloWorld {
+    java: 
+    `class HelloWorld {
         static public void main(String args[]) {
             System.out.println("Hello World");
         }
     }`,
     javascript: `console.log("Hello World")`,
-    cpp: `
-    #include <iostream>
+    cpp: 
+    `#include <iostream>
     int main() {
         std::cout << "Hello World" << std::endl;
     }`,
-    c: `
-    #include <stdio.h>
+    c: 
+    `#include <stdio.h>
     #include <stdlib.h>
     int main(void) {
         puts("Hello World");
         return EXIT_SUCCESS;
     }`,
-    css: `
-    body:before {
+    css: 
+    `body:before {
         content: "Hello World";
     }`,
     shell: `echo Hello World`,
     sql: `SELECT "Hello World";`,
     powershell: `'Hello World'`,
-    html: `
-    <html>
+    html: 
+    `<html>
         <head>
             <title> Hello World </title>
         </head>
@@ -42,7 +42,7 @@ function printCode() {
     let keys = Object.keys(helloWorld);
     index = Math.round(Math.random() * keys.length - 1);
     console.log(helloWorld[keys[index]])
-    console.log(keys.index)
+    console.log(keys[index])
     for(let i = 0; i < helloWorld[keys[index]].length; i++) {
         console.log(helloWorld[keys[index]][i])
     }
